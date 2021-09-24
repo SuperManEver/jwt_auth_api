f00713a8b79131cd18be70cfaee7f981d3f24db5
Nikita Luparev <nikita.luparev@gmail.com>
2021-09-24 14:56:57 +0500

    add users seeds

--- db/seeders/20210924094844-test-users.js
+++ db/seeders/20210924094844-test-users.js
@@ -8,6 +8,18 @@ module.exports = {
         email: 'example@example.com',
         password_hash: '123123',
         username: 'jawosomo'
+      },
+      {
+        id: uuidv4(),
+        email: 'admin@test.com',
+        password_hash: '123123',
+        username: 'dude_slow'
+      },
+      {
+        id: uuidv4(),
+        email: 'guest@main.com',
+        password_hash: '123123',
+        username: 'bobobo'
       }
     ])
   },
