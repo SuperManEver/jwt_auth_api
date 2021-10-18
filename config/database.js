9f5d6f2a1018a62e286f0a66fe3b8e27fb189f34
Nikita Luparev <nikita.luparev@gmail.com>
2021-10-18 06:48:24 +0500

    adjust

--- config/database.js
+++ config/database.js
@@ -2,15 +2,20 @@ const config = require('./index')
 
 const poolMax = parseInt(config.DATABASE_POOL_MAX, 10) || 5
 
+const dialectOptions =
+  config.ENV === 'production'
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    : {}
+
 const commonConfig = {
   url: config.DATABASE_URL,
   dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
+  dialectOptions,
   logging: false,
   pool: {
     max: poolMax,
